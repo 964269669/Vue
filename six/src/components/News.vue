a703811afa999ecaeb031fe5a93c442a94c0737c
964269669 <964269669@qq.com>
2017-06-30 16:18:35 +0800

    six vue2.0 vue-loader

---
+++ six/src/components/News.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>我是新闻页</h3>
+    <ul>
+      <li v-for="(val,index) in news">{{val}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default{
+    name:'news',
+    data(){
+      return {
+        news:['1111','2222','3333']
+      }
+    }
+  }
+</script>
